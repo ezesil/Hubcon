4685391883e5f8dda70e0c04b6de6018ac9b91f7
Ezedev <ezesil96@gmail.com>
2025-05-23 03:44:15 -0300

    Minor name fix

--- HubconAnalyzers.CodeFixes/CodeFixResources.Designer.cs
+++ HubconAnalyzers.CodeFixes/CodeFixResources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace HubconAnalyzer {
+namespace HubconAnalyzers.CodeFixes {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace HubconAnalyzer {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("HubconAnalyzer.CodeFixResources", typeof(CodeFixResources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("HubconAnalyzers.CodeFixes.CodeFixResources", typeof(CodeFixResources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
